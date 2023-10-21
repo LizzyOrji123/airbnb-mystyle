2ddd0726beb4cc785eaaed6c35e03315338646c3
LizzyOrji123 <cegscutie4rill@yahoo.com>
2023-10-21 15:24:50 +0100

    finished up the markers

--- src/App.js
+++ src/App.js
@@ -21,16 +21,31 @@ class App extends Component {
    .then(response => response.json())
    .then((data) => {
     this.setState({
-      flats: data
+      flats: data,
+      selectedFlat: null
     });
   })
   }
 
+  selectFlat =(flat) =>{
+    this.setState({
+      selectedFlat: flat
+    })
+  }
+
   render() {
 
-    const center = {
+    let center = {
       lat : 48.8566,
       lng : 2.3522,
+    }
+
+    if (this.state.selectedFlat) {
+      center = {
+       lat : this.state.selectedFlat.lat,
+       lng : this.state.selectedFlat.lng
+
+      }
     }
 
     return (
@@ -40,7 +55,8 @@ class App extends Component {
           </div>
             <div className="flats">
             {this.state.flats.map((flat) => {
-              return <Flat flat={flat} />
+              return <Flat key={flat.name} flat={flat}
+              selectFlat = {this.selectFlat}/>
             })}
             </div>
         </div>
@@ -49,7 +65,12 @@ class App extends Component {
               center={center}
               zoom={11}>
               {this.state.flats.map((flat) => {
-              return <Marker lat={flat.lat} lng={flat.lng} text={flat.price}/>
+              return <Marker
+                key={flat.name}
+                lat={flat.lat}
+                lng={flat.lng}
+                text={flat.price}
+                selected ={flat === this.state.selectedFlat}/>
               })}
             </GoogleMapReact>
         </div>
